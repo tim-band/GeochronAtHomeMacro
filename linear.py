1d4feb7d22b2aa0ac9c7d1ccd4609fbc99934412
Tim Band <t.band@nnnn.com>
2022-05-06 11:12:50 +0100

    initial commit

---
+++ linear.py
@@ -0,0 +1,103 @@
+﻿# Various linear algebra operations
+# - Vector and matrix multiplication
+# - Procrustean registration; find rigid rotation to match points
+# Usage:
+#
+# import linear
+# reload(linear)
+#
+# m = linear.get_transform(seq, -1)
+# (x,y) = linear.transform(m, seq[i][2], seq[i][3])
+# # (x,y) should be approximately (seq[i][0], seq[i][1])
+
+import math
+
+def find_centroids(fixed_and_rotated):
+    """
+    From a (nonempty) set of quadruplets (x,y,w,z) returns the means of
+    each x, y, w, and z in a quadruplet
+    """
+    n = len(fixed_and_rotated)
+    if n == 0:
+        return (0, 0, 0, 0)
+    xt = 0
+    yt = 0
+    wt = 0
+    zt = 0
+    for (x,y,w,z) in fixed_and_rotated:
+        xt += x
+        yt += y
+        wt += w
+        zt += z
+    return (xt / n, yt / n, wt / n, zt / n)
+
+def find_rotation(fixed_and_rotated):
+    """
+    From a set of quadruplets (x,y,w,z) where the (x,y)s are the fixed points
+    and the (w,z)s are their corresponding rotating points, find the angle that
+    best rotates the rotating points close to the fixed points, as a pair of
+    its sin and cos.
+    """
+    n = 0
+    d = 0
+    for (x,y,w,z) in fixed_and_rotated:
+        n += -w*y - z*x
+        d += -w*x + z*y
+    h = math.hypot(n, d)
+    if h < 0.001:
+        return (0,1)
+    return (n/h, d/h)
+
+def get_transformation(fixed_and_rotated, flip=1):
+    """
+    Returns the transformation matrix.
+    fixed_and_rotated is a sequence of quadruplets (x,y,w,z) where
+    (x,y) are the fixed points and (w,z) are the corresponding
+    points to be rotated by the matrix returned.
+    If flip is -1 the matrix will include a mirroring.
+    The return value is [[r0, r1, tx], [r2, r3, ty]] that minimizes
+    the errors between the (x,y)s and the
+    (r0*w + r1*z + tx, r2*w + r3*z + ty)s
+    """
+    # RF(x-T) = RFx - RFT
+    # F = I or diag(-1, 1), R = (c s / -s c)
+    (mpx,mpy,mx,my) = find_centroids(fixed_and_rotated)
+    far2 = [(x-mpx,y-mpy,flip*(mx-w),z-my) for (x,y,w,z) in fixed_and_rotated]
+    (s,c) = find_rotation(far2)
+    return [
+        [flip*c, -s, -flip*c*mx + s*my + mpx],
+        [flip*s, c, -flip*s*mx - c*my + mpy]
+    ]
+
+def table_to_matrix(table):
+    if not table:
+        return None
+    rows = []
+    for r in range(table.RowCount):
+        column = []
+        for c in range(table.ColumnCount):
+            column.append(float(table.GetValue(r,c)))
+        rows.append(column)
+    return rows
+
+def transform(matrix, x, y):
+    """
+    Returns point (x,y) transformed by matrix
+    """
+    return (
+        x * matrix[0][0] + y * matrix[0][1] + matrix[0][2],
+        x * matrix[1][0] + y * matrix[1][1] + matrix[1][2]
+    )
+
+def inv_transform(matrix, x, y):
+    """
+    Returns point (x,y) transformed by the inverse of matrix,
+    assuming that matrix is a pure rotation, mirroring and translation
+    (that is, an orthogonal matrix)
+    """
+    xt = x - matrix[0][2]
+    yt = y - matrix[1][2]
+    return(
+        xt * matrix[0][0] + y * matrix[1][0],
+        yt * matrix[0][1] + y * matrix[1][1]
+    )
---
+++ workarounds.py
@@ -0,0 +1,123 @@
+﻿# Library of workarounds to Zen macro difficulties
+# Usage:
+#
+# import workarounds
+# reload(workarounds)
+#
+# zen = workarounds.Workarounds(Zen, Zeiss)
+#
+
+import os
+import random
+
+class UserCancelledException(Exception):
+    pass
+
+class Workarounds:
+    def __init__(self, zen, zeiss):
+        self.zen = zen
+        self.zeiss = zeiss
+        self.backlash = -5
+
+    def move_stage(self, x=None, y=None, z=None):
+        """
+        Move the stage and/or focus with backlash correction
+        """
+        if x != None or y != None:
+            if x == None:
+                x = self.zen.Devices.Stage.ActualPositionX
+                xb = x
+            else:
+                xb = x + self.backlash
+            if y == None:
+                y = self.zen.Devices.Stage.ActualPositionY
+                yb = y
+            else:
+                yb = y + self.backlash
+            self.zen.Devices.Stage.MoveTo(xb, yb)
+            self.zen.Devices.Stage.MoveTo(x, y)
+        if z != None:
+            self.zen.Devices.Focus.MoveTo(z + self.backlash)
+            self.zen.Devices.Focus.MoveTo(z)
+
+    def set_focus(self, z):
+        self.move_stage(z=z)
+
+    def show_window(self, window):
+        """
+        Show the window and throw an exception if the user cancels
+        """
+        r = window.Show()
+        if r.HasCanceled:
+            raise UserCancelledException("Operation cancelled")
+        return r
+
+    def switch_to_locate_tab(self):
+        self.zen.Application.LeftToolArea.SwitchToTab(
+            self.zeiss.Micro.Scripting.Research.ZenToolTab.LocateTab
+        )
+
+    def set_hardware(self, hardware_setting=None, camera_setting=None):
+        """
+        Set hardware and/or camera setting
+        """
+        self.switch_to_locate_tab()
+        if type(hardware_setting) is str:
+            hs = self.zen.Application.HardwareSettings.GetByName(hardware_setting)
+            if hs == None:
+                raise Exception('No such harware setting {0}'.format(hardware_setting))
+            hardware_setting = hs
+        if hardware_setting != None:
+            self.zen.Devices.ApplyHardwareSetting(hardware_setting)
+        if type(camera_setting) == str:
+            cs = self.zen.Acquisition.CameraSettings.GetByName(camera_setting)
+            if cs == None:
+                raise Exception('No such camera setting {0}'.format(camera_setting))
+            camera_setting = cs
+        if camera_setting != None:
+            self.zen.Acquisition.ActiveCamera.ApplyCameraSetting(camera_setting)
+
+    def show_live(self):
+        self.switch_to_locate_tab()
+        live = self.zen.Acquisition.StartLive()
+        self.zen.Application.Documents.ActiveDocument = live
+        return live
+
+    def autofocus(self, experiment, timeoutSeconds=30):
+        """
+        Autofocus using the settings in the experiment passed
+        """
+        self.switch_to_locate_tab()
+        if type(experiment) is str:
+            e = self.zeiss.Micro.Scripting.ZenExperiment(experiment)
+            if e == None:
+                raise Exception('No such acquisition experiment {0}'.format(experiment))
+            experiment = e
+        self.show_live()
+        result = self.zen.Acquisition.FindAutofocus(experiment, timeoutSeconds=timeoutSeconds)
+
+    def discard_changes(self, e):
+        """
+        Discard a document without getting input from the user
+        """
+        dummy_name = 'dummy{0:04d}'.format(random.randint(0,9999))
+        e.SaveAs(dummy_name)
+        e.Delete()
+        e.Close()
+
+    def save_as(self, zen_image, path, overwrite=False):
+        """
+        Saves the image with the given file path.
+        Returns True on success, False if the file already exists and
+        overwrite is False, throws an exception on any other error
+        """
+        if os.path.exists(path):
+            if not overwrite:
+                return False
+            os.unlink(path)
+        if not zen_image.Save():
+            return False
+        src = zen_image.FileName
+        zen_image.Close()
+        os.rename(src, path)
+        return True
