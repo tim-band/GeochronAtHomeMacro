4a69319201a68427691819c339b3df2f1376687c
Tim Band <t.band@nnnn.com>
2023-01-13 16:35:18 +0000

    hardware and camera settings in code

---
+++ hardware_settings.py
@@ -0,0 +1,47 @@
+﻿settings = {
+'reflected': {
+    'MTBRLHalogenLamp': {
+        'Intensity': 50
+    },
+    'MTBRLShutter': {
+        'IsClosed': False
+    },
+    'MTBRLApertureStop': {
+        'Position': 33
+    },
+    'MTBTLHalogenLamp': {
+        'LampMode': 'Standby'
+    },
+    'MTBRLFieldStop': {
+        'Position': 85
+    },
+    'MTBRLTLSwitch': {
+        'PositionName': 'RLTLSwitch.RL'
+    },
+    'MTBReflectorChanger': {
+        'PositionName': 'Reflector.424928-9901-000'
+    },
+    'MTBRLFilterChanger1': {
+        'PositionName': 'ND 6'
+    },
+    'MTBRLFilterChanger2': {
+        'PositionName': 'ND 100'
+    }
+},
+'transmitted': {
+    'MTBTLHalogenLamp': {
+        'Intensity': 10.8
+    },
+    'MTBRLHalogenLamp': {
+        'LampMode': 'Standby'
+    },
+    'MTBRLShutter': {
+        'IsClosed': True
+    },
+    'MTBReflectorChanger': {
+        'PositionName': 'Reflector.none'
+    },
+    'MTBRLTLSwitch': {
+        'PositionName': 'RLTLSwitch.TL'
+    }
+}}
